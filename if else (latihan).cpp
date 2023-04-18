a8480e59887575eb0d826f395923f3fa5432e74b
lutfiputra213 <130426002+lutfiputra213@users.noreply.github.com>
2023-04-18 11:26:48 +0700

    Add files via upload

---
+++ if else (latihan).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a=10,b=20;
+	
+	cout<<"contoh if else statement"<<endl;
+	
+	if(a>b){
+		cout<<"a lebih besar daripada b"<<endl;
+	}else{
+		cout<<"b lebih besar daripada a"<<endl;
+	}
+	
+	return 0;
+}
